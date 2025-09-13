1f99aec8d00ec424c28479a6a4efaf6e82d4163c
Asunar5398 <aryansunar5398@gmail.com>
2025-09-13 22:13:51 +0530

    Add files via upload

---
+++ assignment1 task1.py
@@ -0,0 +1,13 @@
+# num1=int(input("Enter first number:"))
+# num2=int(input("Enter second number:"))
+# sum=num1+num2
+# difference=num1-num2
+# product=num1*num2
+# quotient=num1/num2
+# p=print("Addition =",sum)
+# print("Subtraction =",difference)
+# print("Product =",product)
+# print("Division =",quotient)
+
+
+
